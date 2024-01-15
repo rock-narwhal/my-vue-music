e6e53f184c87e46cf880f652c1533a69d71371bb
rock-narwhal <1987747418@qq.com>
2024-01-15 21:06:20 +0800

    榜单页

--- src/components/header/TopListBanner.vue
+++ src/components/header/TopListBanner.vue
@@ -4,18 +4,18 @@
       <div class="cover pointer">
         <img :src="topItem.coverImgUrl + '?param=300y300'">
         <div class="play-btn pointer" @click.stop="clickPlay(topItem.id)">
-          <i class="iconfont font-16 icon-bofang"></i>
+          <i class="iconfont font-18 icon-bofang"></i>
         </div>
       </div>
       <ul class="right-list">
         <li v-for="(subItem,subIndex) in detailList[index]" :key="subItem.id">
-          <span>{{ subIndex + 1 }}</span>
-          <span>{{ subItem.name }}</span>
-          <span>{{ subItem.ar[0].name }}</span>
+          <div :class="{hot : subIndex < 3}">{{ subIndex + 1 }}</div>
+          <div>{{ subItem.name }}</div>
+          <div class="sub-right">{{ subItem.ar[0].name }}</div>
         </li>
-        <button class="no-btn">
+        <li class="no-btn" @click="viewMore(topItem.id)">
           查看全部 <i class="el-icon-arrow-right"></i>
-        </button>
+        </li>
       </ul>
     </div>
   </div>
@@ -39,16 +39,20 @@ export default {
   methods: {
     clickPlay(id) {
       console.log('TopListBanner :clickPlay', id)
+      this.$emit('clickPlay',id)
+    },
+    viewMore(id){
+      this.$emit('viewMore',id)
     },
     async getDetailList(id) {
-      console.log('TopListBanner: loadDetailList',id)
+      console.log('TopListBanner: loadDetailList', id)
       const res = await getPlayListDetail(id)
       if (res.code !== 200) return []
       this.detailList.push(res.playlist.tracks.splice(0, 5))
     }
   },
   watch:{
-    list(){
+    list(){ //等this.list的数据刷新后再获取detailList的数据
       console.log("watch list",this.list)
       this.detailList = []
       this.list.forEach((item) =>{
@@ -56,20 +60,81 @@ export default {
       })
     }
   },
+  created() {
+    console.log('TopListBanner created', this.list)
+    this.list.forEach((item) => {
+      this.getDetailList(item.id)
+    })
+  },
   mounted() {
     //子组件mounted比父组件早，
   }
 }
 </script>
 <style scoped lang="less">
-.top-banner{
+.top-banner {
   width: 100%;
-  padding: 0 200px;
-  .banner-item{
+
+  .banner-item {
     display: flex;
-    .cover{
-      width: 15%;
+    margin-bottom: 30px;
+    .cover {
+      width: 190px;
+      height: 190px;
       position: relative;
+
+      img{
+        width: 100%;
+        height: auto;
+        border-radius: 8px;
+      }
+
+      .play-btn {
+        position: absolute;
+        height: 40px;
+        width: 40px;
+        text-align: center;
+        line-height: 40px;
+        top: 60px;
+        left: 60px;
+        background-color: white;
+        border-radius: 50%;
+        color: #ec4141;
+        display: none;
+      }
+
+      &:hover .play-btn {
+        display: block;
+      }
+    }
+    .right-list{
+      width: 100%;
+      margin-left: 30px;
+      display: flex;
+      flex-direction: column;
+      li{
+        height: 32px;
+        width: 100%;
+        line-height: 32px;
+        border-radius: 3px;
+        .hot{
+          color: #ec4141;
+        }
+        &:nth-child(odd){
+          background-color: #eeeeee;
+        }
+        &:hover{
+          background-color: #dadada;
+        }
+        div{
+          margin-left: 10px;
+          float: left;
+        }
+        div.sub-right{
+          float: right;
+          margin-right: 10px;
+        }
+      }
     }
   }
 }
--- src/views/home/TopList.vue
+++ src/views/home/TopList.vue
@@ -1,18 +1,34 @@
 <template>
 <!--  排行榜页面-->
   <div class="container">
-    <TopListBanner :list="officialTopList"></TopListBanner>
+    <div class="list-title">官方榜</div>
+    <TopListBanner
+        @viewMore="toListDetail"
+        @clickPlay="clickPlay"
+        :list="officialTopList"></TopListBanner>
+    <div class="list-title">全球榜</div>
+    <ImgList type="playlist"
+             :list="globalTopList"
+             @clickPlay="clickPlay"
+             @clickImg="toListDetail"
+    >
+      <template v-slot="{item}">
+        {{item.name}}
+      </template>
+    </ImgList>
   </div>
 
 </template>
 <script>
 import {getTopList} from "@/api/api_toplist";
 import TopListBanner from "@/components/header/TopListBanner.vue";
+import ImgList from "@/components/list/ImgList.vue";
+import playList from "@/views/home/PlayList.vue";
 
 export default {
   name: "TopList",
   components:{
-    TopListBanner,
+    TopListBanner,ImgList,
   },
   data(){
     return {
@@ -24,17 +40,21 @@ export default {
     this.getTopList()
   },
   computed:{
+    playList() {
+      return playList
+    },
     // 官方榜单
     officialTopList(){
+      console.log('officialTopList')
       if (this.topList.length > 0) {
-        return this.topList.splice(0, 4)
+        return this.topList.slice(0, 4)
       } else {
         return this.topList
       }
     },
     globalTopList(){
       if(this.topList.length > 0){
-        return this.topList.splice(4)
+        return this.topList.slice(4)
       }
       return this.topList
     }
@@ -46,10 +66,25 @@ export default {
       if(res.code !== 200) return
       console.log("topList: ",res.list)
       this.topList = res.list
+    },
+    //播放歌单
+    clickPlay(id){
+      console.log('TopList: clickPlay',id)
+    },
+    //跳转到榜单详情
+    toListDetail(id){
+      console.log('TopList: toListDetail',id)
     }
   }
 }
 </script>
 <style scoped lang="less">
-
+.container{
+  padding: 0 200px;
+  .list-title{
+    margin: 20px 0;
+    font-size: 20px;
+    font-weight: bolder;
+  }
+}
 </style>
