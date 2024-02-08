1a5ec7970b0cfe3f41d9406484a7e537edd6d988
rock-narwhal <1987747418@qq.com>
2024-02-08 22:13:04 +0800

    el-table 窗口大小调整导致报错问题修复

--- src/element/index.js
+++ src/element/index.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import 'element-ui/lib/theme-chalk/index.css'
-import ElementUI from 'element-ui'
+import ElementUI,{Table} from 'element-ui'
+
+const fixElTableErr = table =>{
+    const oldResizeListener = table.methods.resizeListener
+    table.methods.resizeListener = function (){
+        window.requestAnimationFrame(oldResizeListener.bind(this))
+    }
+}
+fixElTableErr(Table)
 
 Vue.use(ElementUI)
