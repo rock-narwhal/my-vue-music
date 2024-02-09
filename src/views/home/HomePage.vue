57527a96bc236fadb3277347c5c4e4742bf916e6
rock-narwhal <1987747418@qq.com>
2024-02-09 10:36:24 +0800

    主页flex布局

--- src/views/home/HomePage.vue
+++ src/views/home/HomePage.vue
@@ -3,11 +3,16 @@
     <div>
       <tab-menu :menuList="routeList" mode="route" style="margin: 0 30px"></tab-menu>
     </div>
-    <div>
-      <transition name="el-fade-in">
-        <router-view></router-view>
-      </transition>
+    <div class="content-wrap">
+      <div class="space-wrap"></div>
+      <div class="content-area">
+        <transition name="el-fade-in">
+          <router-view></router-view>
+        </transition>
+      </div>
+      <div class="space-wrap"></div>
     </div>
+
   </div>
 </template>
 
@@ -17,19 +22,39 @@ import TabMenu from "@/components/menu/TabMenu";
 export default {
   name: "HomePage",
   components: {TabMenu},
-  comments:{
+  comments: {
     TabMenu,
   },
-  data(){
-    return{
-      routeList:[
-        { path: '/homePage/personalrecom', name: '个性推荐' },
-        { path: '/homePage/playlist', name: '歌单' },
-        { path: '/homePage/toplist', name: '排行榜' },
-        { path: '/homePage/artistlist', name: '歌手' },
-        { path: '/homePage/latest', name: '最新音乐' }
+  data() {
+    return {
+      routeList: [
+        {path: '/homePage/personalrecom', name: '个性推荐'},
+        {path: '/homePage/playlist', name: '歌单'},
+        {path: '/homePage/toplist', name: '排行榜'},
+        {path: '/homePage/artistlist', name: '歌手'},
+        {path: '/homePage/latest', name: '最新音乐'}
       ]
     }
   }
 }
 </script>
+
+<style scoped lang="less">
+.content-wrap {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+  flex-wrap: nowrap;
+}
+
+.content-area {
+  width: 1100px;
+  box-sizing: border-box;
+}
+
+.space-wrap {
+  flex-shrink: 1;
+  min-width: 30px;
+}
+</style>
